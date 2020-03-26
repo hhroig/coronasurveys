5b14c6c9ef5972655b608a66c0c2cfa5702586b0
Oluwasegun Ojo <statimatician@gmail.com>
2020-03-26 12:02:13 +0100

    adapt coronasurvey script for filename and country population

---
+++ code/corona_surveys_estimate.R
@@ -0,0 +1,69 @@
+estimate_cases <- function(file_path = "../data/ES-06-20200322-20200323.csv", country_population = 46754778,
+                           max_ratio = .3, correction_factor = 0.65) {
+  # read data
+  dt <- read.csv(file_path)
+  names(dt) <- c("date","region","reach","cases")
+  n_inital_response <- nrow(dt)
+  
+  # get name of csv file for later labelling of outliers removed files
+  file_names_com <- unlist(strsplit(file_path, split = "/"))
+  file_name <- file_names_com[length(file_names_com)] # select the last path of file_path
+  
+  
+  # remove outliers from the reach column
+  reach_cutoff <-min(boxplot(dt$reach, plot = F)$out)
+  if(sum(dt$reach >= reach_cutoff) > 0 ){
+    write.table(dt[dt$reach >= reach_cutoff, ],
+                file = paste0("outliers_removed/", file_name, "_", "outliers_reach.txt"),
+                append = T) # write out outliers from reach column to the ouliers removed folder
+    n_reach_outliers <- sum(dt$reach >= reach_cutoff) #number of outliers removed based on reach
+    dt <- dt[dt$reach < reach_cutoff, ]
+  }else{
+    n_reach_outliers <- 0
+  }
+  
+  
+  # remove outliers based on max ratio of 0.3
+  dt$ratio <- dt$cases/dt$reach
+  if(sum(dt$ratio >= max_ratio) > 0 ){
+    write.table(dt[dt$ratio >= max_ratio, ],
+                file = paste0("outliers_removed/", file_name, "_", "outliers_max_ratio.txt"),
+                append = T) # write out outliers based on max_Ratio
+    n_maxratio_outliers <- sum(dt$ratio >= max_ratio)
+    dt <- dt[dt$ratio < maxratio, ]
+  }else{
+    n_maxratio_outliers <- 0
+  }
+  
+  
+  
+  # estimate infection cases
+  mean_cases <- mean(dt$cases)
+  mean_reach <- mean(dt$reach)
+  cases_per_reach <-sum(dt$cases)/sum(dt$reach)
+  
+  naif_cases<- population * cases_per_reach * correction_factor
+  
+  return(list(estimated_cases = naif_cases,
+              cases_breakdown = table(dt$cases),
+              cases_breakdown_region = table(dt$region, dt$cases),
+              n_reach_outliers = n_reach_outliers,
+              n_maxratio_outliers = n_maxratio_outliers,
+              n_inital_response = n_inital_response,
+              n_final_response = nrow(dt)))
+}
+
+
+# test for spain
+
+estimates_spain_pool6 <- estimate_cases(file_path = "../data/ES-06-20200322-20200323.csv", country_population = 46754778)
+
+# check results
+estimates_spain_pool6$estimated_cases 
+estimates_spain_pool6$cases_breakdown # table of frequency of cases, good for bar chart
+estimates_spain_pool6$cases_breakdown_region # table of frequency of cases, by region
+estimates_spain_pool6$n_reach_outliers # number of ouliers removed based on reach column
+estimates_spain_pool6$n_maxratio_outliers # number of outliers removed based on max ratio allowed
+estimates_spain_pool6$n_inital_response # number of rows of original data
+estimates_spain_pool6$n_final_response # nrows of data after used for estimation after outliers removed.
+
