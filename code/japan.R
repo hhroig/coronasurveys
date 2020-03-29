fd5433bbbc3a62bedb0b49aaabc0a0bcb2ddb540
CBaquero <cbm@di.uminho.pt>
2020-03-29 10:59:39 +0100

    japan data plot

--- code/japan.R
+++ code/japan.R
@@ -2,29 +2,10 @@ source("corona_surveys_estimate.R")
 
 # test for spain
 
-estimates_spain_pool6 <- estimate_cases(file_path = "../data/ES-06-20200322-20200323.csv", country_population = 46754778)
-estimates_spain_pool7 <- estimate_cases(file_path = "../data/ES-07-20200324-20200325.csv", country_population = 46754778)
-estimates_spain_pool8 <- estimate_cases(file_path = "../data/ES-08-20200326-20200327.csv", country_population = 46754778)
+estimates_japan_pool1 <- estimate_cases(file_path = "../data/JP-01-20200326-20200328.csv", country_population = 1.27e+08)
 
 # check results
-estimates_spain_pool6$mean_cases 
-estimates_spain_pool6$mean_reach
-estimates_spain_pool6$cases_per_reach
-estimates_spain_pool6$estimated_cases 
-#estimates_spain_pool6$cases_breakdown # table of frequency of cases, good for bar chart
-#estimates_spain_pool6$cases_breakdown_region # table of frequency of cases, by region
-#estimates_spain_pool6$n_reach_outliers # number of ouliers removed based on reach column
-#estimates_spain_pool6$n_maxratio_outliers # number of outliers removed based on max ratio allowed
-#estimates_spain_pool6$n_inital_response # number of rows of original data
-#estimates_spain_pool6$n_final_response # nrows of data after used for estimation after outliers removed.
-
-#
-estimates_spain_pool7$mean_cases 
-estimates_spain_pool7$mean_reach
-estimates_spain_pool7$cases_per_reach
-estimates_spain_pool7$estimated_cases 
-
-estimates_spain_pool8$mean_cases 
-estimates_spain_pool8$mean_reach
-estimates_spain_pool8$cases_per_reach
-estimates_spain_pool8$estimated_cases 
+estimates_japan_pool1$mean_cases 
+estimates_japan_pool1$mean_reach
+estimates_japan_pool1$cases_per_reach
+estimates_japan_pool1$estimated_cases 
