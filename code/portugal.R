37062f7c22d554aeaded76140c3d3929ea3a528d
CBaquero <cbm@di.uminho.pt>
2020-03-31 16:17:36 +0100

    pt scripts

--- code/portugal.R
+++ code/portugal.R
@@ -4,6 +4,7 @@ source("corona_surveys_estimate.R")
 
 estimates_pt_pool3 <- estimate_cases(file_path = "../data/PT-03-20200322-20200323.csv", country_population = 10261075, correction_factor = 1)
 estimates_pt_pool4 <- estimate_cases(file_path = "../data/PT-04-20200324-20200325.csv", country_population = 10261075, correction_factor = 1)
+estimates_pt_pool5 <- estimate_cases(file_path = "../data/PT-05-20200328-20200329.csv", country_population = 10261075, correction_factor = 1)
 
 # check results
 estimates_pt_pool3$mean_cases 
@@ -16,3 +17,9 @@ estimates_pt_pool4$mean_cases
 estimates_pt_pool4$mean_reach
 estimates_pt_pool4$cases_per_reach
 estimates_pt_pool4$estimated_cases 
+
+#
+estimates_pt_pool5$mean_cases 
+estimates_pt_pool5$mean_reach
+estimates_pt_pool5$cases_per_reach
+estimates_pt_pool5$estimated_cases 
