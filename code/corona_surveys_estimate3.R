3e6d05fe091c59b938559124f2ec419472e2d6c0
Oluwasegun Ojo <oluwasegun.ojo@imdea.org>
2020-05-18 19:22:39 +0200

    revert to previous estimates

--- code/corona_surveys_estimate3.R
+++ code/corona_surveys_estimate3.R
@@ -379,34 +379,36 @@ plot_estimates <- function(country_geoid = "ES",
         rosa_estimate2 <- full_join(rosa_estimate, survey_gforms_estimate2) %>%
           select(names(survey_gforms_estimate))
         survey_gforms_estimate <- rosa_estimate2
-      } else if (country_geoid == "PT"){
-        rosa_estimate <- get_portugal_region_based_rosa(write_file = F)
-        cols_to_remove <- names(survey_gforms_estimate)[names(survey_gforms_estimate) %in% names(rosa_estimate)]
-        cols_to_remove <- cols_to_remove[-1]
-        survey_gforms_estimate2 <- survey_gforms_estimate %>%
-          select(-cols_to_remove)
-        rosa_estimate2 <- full_join(rosa_estimate, survey_gforms_estimate2) %>%
-          select(names(survey_gforms_estimate))
-        survey_gforms_estimate <- rosa_estimate2
-      } else if(country_geoid == "IT"){
-        rosa_estimate <- get_italy_region_based_rosa(write_file = F)
-        cols_to_remove <- names(survey_gforms_estimate)[names(survey_gforms_estimate) %in% names(rosa_estimate)]
-        cols_to_remove <- cols_to_remove[-1]
-        survey_gforms_estimate2 <- survey_gforms_estimate %>%
-          select(-cols_to_remove)
-        rosa_estimate2 <- full_join(rosa_estimate, survey_gforms_estimate2) %>%
-          select(names(survey_gforms_estimate))
-        survey_gforms_estimate <- rosa_estimate2
-      } else if(country_geoid == "UA"){
-        rosa_estimate <- get_ukraine_region_based_rosa(write_file = F)
-        cols_to_remove <- names(survey_gforms_estimate)[names(survey_gforms_estimate) %in% names(rosa_estimate)]
-        cols_to_remove <- cols_to_remove[-1]
-        survey_gforms_estimate2 <- survey_gforms_estimate %>%
-          select(-cols_to_remove)
-        rosa_estimate2 <- full_join(rosa_estimate, survey_gforms_estimate2) %>%
-          select(names(survey_gforms_estimate))
-        survey_gforms_estimate <- rosa_estimate2
-      }
+      } 
+      
+      # else if (country_geoid == "PT"){
+      #   rosa_estimate <- get_portugal_region_based_rosa(write_file = F)
+      #   cols_to_remove <- names(survey_gforms_estimate)[names(survey_gforms_estimate) %in% names(rosa_estimate)]
+      #   cols_to_remove <- cols_to_remove[-1]
+      #   survey_gforms_estimate2 <- survey_gforms_estimate %>%
+      #     select(-cols_to_remove)
+      #   rosa_estimate2 <- full_join(rosa_estimate, survey_gforms_estimate2) %>%
+      #     select(names(survey_gforms_estimate))
+      #   survey_gforms_estimate <- rosa_estimate2
+      # } else if(country_geoid == "IT"){
+      #   rosa_estimate <- get_italy_region_based_rosa(write_file = F)
+      #   cols_to_remove <- names(survey_gforms_estimate)[names(survey_gforms_estimate) %in% names(rosa_estimate)]
+      #   cols_to_remove <- cols_to_remove[-1]
+      #   survey_gforms_estimate2 <- survey_gforms_estimate %>%
+      #     select(-cols_to_remove)
+      #   rosa_estimate2 <- full_join(rosa_estimate, survey_gforms_estimate2) %>%
+      #     select(names(survey_gforms_estimate))
+      #   survey_gforms_estimate <- rosa_estimate2
+      # } else if(country_geoid == "UA"){
+      #   rosa_estimate <- get_ukraine_region_based_rosa(write_file = F)
+      #   cols_to_remove <- names(survey_gforms_estimate)[names(survey_gforms_estimate) %in% names(rosa_estimate)]
+      #   cols_to_remove <- cols_to_remove[-1]
+      #   survey_gforms_estimate2 <- survey_gforms_estimate %>%
+      #     select(-cols_to_remove)
+      #   rosa_estimate2 <- full_join(rosa_estimate, survey_gforms_estimate2) %>%
+      #     select(names(survey_gforms_estimate))
+      #   survey_gforms_estimate <- rosa_estimate2
+      # }
     
     }
    
