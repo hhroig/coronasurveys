73372985a5ba5158827113fa2363ccc92c1b2b8f
Oluwasegun Ojo <statimatician@gmail.com>
2020-05-26 14:12:04 +0200

    add provincial-regional-country-based-estimate for Spain

--- code/test_centralized_agg.R
+++ code/test_centralized_agg.R
@@ -1,29 +1,10 @@
-provinces_and_codes <- readxl::read_excel("regions-tree-population.xlsx")
-
-# test_brazil_regional_estimate <- function(max_ratio = .3){
-#   cat("Generating region based estimates for Brazil \n")
-#   dt <- read.csv("../data/aggregate/BR-aggregate.csv", as.is = T)
-#   dt_region <- provinces_and_codes[provinces_and_codes$countrycode == "BR", ]
-#   names(dt) <- tolower(names(dt))
-#   dt <- dt[, c("timestamp","region","reach","cases", "iso.3166.1.a2", "iso.3166.2")]
-#   dt$date <- substr(dt$timestamp, 1, 10)
-#   # outlier detection
-#   dt <- dt[!is.na(dt$reach),]
-#   reach_cutoff <- boxplot.stats(dt$reach)$stats[5] # changed cutoff to upper fence
-#   dt <- dt[dt$reach <= reach_cutoff, ]
-#   
-#   dt$ratio <- dt$cases/dt$reach    # remove outlier based on 0.3
-#   dt <- dt[is.finite(dt$ratio), ]  # discard cases with zero reach
-#   dt <- dt[dt$ratio <= max_ratio, ]
-#   dt$iso.3166.2[dt$iso.3166.2 == ""] <- "PT"
-#   #
-#   
-#   
-# }
-
-test_spain_regional_estimate <- function(province = T, district = F,
-                                         W = 90, alpha = 0.0001,
-                                         max_ratio = .3){
+spain_provincial_regional_estimate <- function(province = T,
+                                         district = F,
+                                         W = 90,
+                                         alpha = 0.0001,
+                                         max_ratio = .3,
+                                         provinces_and_codes = readxl::read_excel("regions-tree-population.xlsx"),
+                                         write_file = T){
   cat("Generating region based estimates for Spain \n")
   dt <- read.csv("../data/aggregate/ES-aggregate.csv", as.is = T)
   dt_region <- provinces_and_codes[provinces_and_codes$countrycode == "ES", ]
@@ -47,7 +28,7 @@ test_spain_regional_estimate <- function(province = T, district = F,
   dates <- unique(dt$date)
   
 
-  dt_region2 <- dt_region[, c("countrycode",  "regioncode",   "provincecode", "population")] 
+  dt_region2 <- dt_region[, c("countrycode",  "regioncode",   "provincecode", "population")] ## bring autonomous cities code to lowest level
   r_c <-  c()
   r_r <-  c()
   I_c_estprop <- c()
@@ -138,13 +119,13 @@ test_spain_regional_estimate <- function(province = T, district = F,
     # aggregate provincia data into regional data
     if(province == T){
       # population variable refer to population at lowest level
-      dt_estimate <- merge(dtregs, dtprovs, all = T, by = c("countrycode", "regioncode")) 
+      dt_est_prov_reg <- merge(dtregs, dtprovs, all = T, by = c("countrycode", "regioncode")) 
       # go over regions and computed aggregated means
       estprop_regs_rhs <- c()
       meanprop_regs_rhs <- c()
       sumreach_regs_rhs <- c()
-      for (i in unique(dt_estimate$regioncode)) {
-        dt_est_reg <- na.omit(dt_estimate[dt_estimate$regioncode == i, ])
+      for (i in unique(dt_est_prov_reg$regioncode)) {
+        dt_est_reg <- na.omit(dt_est_prov_reg[dt_est_prov_reg$regioncode == i, ])
         weight <- dt_est_reg$population/dt_est_reg$population_region
         estprop_regs_rh <- sum(weight * dt_est_reg$estprop_provs)
         meanprop_regs_rh <- sum(weight * dt_est_reg$meanprop_provs)
@@ -153,23 +134,23 @@ test_spain_regional_estimate <- function(province = T, district = F,
         estprop_regs_rhs <- c(estprop_regs_rhs, estprop_regs_rh)
         meanprop_regs_rhs <- c(meanprop_regs_rhs, meanprop_regs_rh)
       }
-      dt_regs_rhs <- data.frame(regioncode = unique(dt_estimate$regioncode),
+      dt_regs_rhs <- data.frame(regioncode = unique(dt_est_prov_reg$regioncode),
                                estprop_regs_rhs = estprop_regs_rhs,
                                meanprop_regs_rhs = meanprop_regs_rhs,
                                sumreach_regs_rhs = sumreach_regs_rhs)
       
-      dt_estimate <- merge(dt_estimate, dt_regs_rhs, by = c("regioncode"))
-      dt_estimate$meanprop_regs_agg <- ((dt_estimate$sumreach_regs/(dt_estimate$sumreach_regs+dt_estimate$sumreach_regs_rhs)) *
-        dt_estimate$meanprop_regs) + ((dt_estimate$sumreach_regs_rhs/(dt_estimate$sumreach_regs+dt_estimate$sumreach_regs_rhs)) *
-                                        dt_estimate$meanprop_regs_rhs)
+      dt_est_prov_reg <- merge(dt_est_prov_reg, dt_regs_rhs, by = c("regioncode"))
+      dt_est_prov_reg$meanprop_regs_agg <- ((dt_est_prov_reg$sumreach_regs/(dt_est_prov_reg$sumreach_regs + dt_est_prov_reg$sumreach_regs_rhs)) *
+        dt_est_prov_reg$meanprop_regs) + ((dt_est_prov_reg$sumreach_regs_rhs/(dt_est_prov_reg$sumreach_regs+dt_est_prov_reg$sumreach_regs_rhs)) *
+                                        dt_est_prov_reg$meanprop_regs_rhs)
       
-      dt_estimate$estprop_regs_agg <- ((dt_estimate$sumreach_regs/(dt_estimate$sumreach_regs+dt_estimate$sumreach_regs_rhs)) *
-                                     dt_estimate$estprop_regs) + ((dt_estimate$sumreach_regs_rhs/(dt_estimate$sumreach_regs+dt_estimate$sumreach_regs_rhs)) *
-                                                                     dt_estimate$estprop_regs_rhs)
+      dt_est_prov_reg$estprop_regs_agg <- ((dt_est_prov_reg$sumreach_regs/(dt_est_prov_reg$sumreach_regs+dt_est_prov_reg$sumreach_regs_rhs)) *
+                                     dt_est_prov_reg$estprop_regs) + ((dt_est_prov_reg$sumreach_regs_rhs/(dt_est_prov_reg$sumreach_regs+dt_est_prov_reg$sumreach_regs_rhs)) *
+                                                                     dt_est_prov_reg$estprop_regs_rhs)
     }else{
-      dt_estimate <- dtregs
-      dt_estimate$meanprop_regs_agg <- dt_estimate$meanprop_regs
-      dt_estimate$estprop_regs_agg <- dt_estimate$estprop_regs
+      dt_est_prov_reg <- dtregs
+      dt_est_prov_reg$meanprop_regs_agg <- dt_est_prov_reg$meanprop_regs
+      dt_est_prov_reg$estprop_regs_agg <- dt_est_prov_reg$estprop_regs
     }
     
     
@@ -183,8 +164,8 @@ test_spain_regional_estimate <- function(province = T, district = F,
                           sumreach_country = sumreach_country,
                           estprop_country = estprop_country,
                           meanprop_country = meanprop_country)
-    dtcounts <- by(dt_estimate,
-                 list(dt_estimate$countrycode, dt_estimate$regioncode), # groupby country and region and compute population
+    dt_est_reg_count <- by(dt_est_prov_reg,
+                 list(dt_est_prov_reg$countrycode, dt_est_prov_reg$regioncode), # groupby country and region and compute population
                  function(x){
                    data.frame(countrycode = unique(x$countrycode),
                               regioncode = unique(x$regioncode), 
@@ -194,23 +175,22 @@ test_spain_regional_estimate <- function(province = T, district = F,
                               sumreach_regs = unique(x$sumreach_regs),
                               stringsAsFactors = F)
                  })
-    dtcounts <- do.call(rbind, dtcounts)
-    weight <- dtcounts$population_region/sum(dtcounts$population_region)
-    estprop_country_rh <- sum(weight * dtcounts$estprop_regs_agg)
-    meanprop_country_rh <- sum(weight * dtcounts$meanprop_regs_agg)
+    dt_est_reg_count <- do.call(rbind, dt_est_reg_count)
+    weight <- dt_est_reg_count$population_region/sum(dt_est_reg_count$population_region)
+    estprop_country_rh <- sum(weight * dt_est_reg_count$estprop_regs_agg, na.rm = T)
+    meanprop_country_rh <- sum(weight * dt_est_reg_count$meanprop_regs_agg, na.rm = T)
+    sumreach_country_rh <-  sum(dt_est_reg_count$sumreach_regs)
     
     
     
-    meanprop_counts_agg <- ((sumreach_country/(sumreach_country + sum(dtcounts$sumreach_regs))) *
-                                          meanprop_country) + ((sum(dtcounts$sumreach_regs)/(sumreach_country + sum(dtcounts$sumreach_regs))) *
-                                                                 meanprop_country_rh)
+    meanprop_counts_agg <- ((sumreach_country/(sumreach_country + sumreach_country_rh)) * meanprop_country) + 
+      ((sumreach_country_rh/(sumreach_country + sumreach_country_rh)) * meanprop_country_rh)
     
-    estprop_counts_agg <- ((sumreach_country/(sumreach_country + sum(dtcounts$sumreach_regs))) *
-                                         estprop_country) + ((sum(dtcounts$sumreach_regs)/(sumreach_country + sum(dtcounts$sumreach_regs))) *
-                                                               estprop_country_rh)
+    estprop_counts_agg <- ((sumreach_country/(sumreach_country + sumreach_country_rh)) * estprop_country) + 
+      ((sumreach_country_rh/(sumreach_country + sumreach_country_rh)) * estprop_country_rh)
     
     r_c <-  c(r_c, sumreach_country)
-    r_r <-  c(r_r, sum(dtcounts$sumreach_regs))
+    r_r <-  c(r_r, sumreach_country_rh)
     I_c_estprop <- c(I_c_estprop, estprop_country)
     I_c_meanprop <- c(I_c_meanprop, meanprop_country)
     I_r_estprop <- c(I_r_estprop, estprop_country_rh)
@@ -227,11 +207,16 @@ test_spain_regional_estimate <- function(province = T, district = F,
                                       I_r_estprop = I_r_estprop, 
                                       I_r_meanprop = I_r_meanprop,
                                       estprop_country_agg = estprop_country_agg,
-                                      meanprop_country_agg = meanprop_country_agg,
-                                        )
-  cat("writing the region based estimate for Spain..\n")
-  write.csv(region_based_estimate, paste0("../data/PlotData/ES_regional_estimates/region_based_estimates/",
-                                          "ES-region-based-estimate_province_region_country_agg.csv"))
-  
-  
+                                      meanprop_country_agg = meanprop_country_agg)
+  if(write_file == T){
+    cat("Writing the region based estimate for Spain..\n")
+    write.csv(region_based_estimate, paste0("../data/PlotData/ES_regional_estimates/region_based_estimates/",
+                                            "ES-province-region-country-based-estimate.csv"))
+  }
+  else{
+    return(region_based_estimate)
+  }
+
 }
+
+spain_provincial_regional_estimate(write_file = T)
