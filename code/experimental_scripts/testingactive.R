9bf6c228a60b883e3fa92d73510b1575919719ac
antonio_fernandez <antonio.fernandez@imdea.org>
2020-07-24 05:10:44 +0200

    active

--- code/experimental_scripts/testingactive.R
+++ code/experimental_scripts/testingactive.R
@@ -1,6 +1,7 @@
 
 library("dplyr")
 library("data.table")
+library("caTools")
 
 ccfrdata <- read.csv("../../data/estimates-ccfr-based/PlotData/ES-estimate.csv")
 casedata <- read.csv("../../data/estimates-confirmed/PlotData/ES-estimate.csv")
@@ -17,7 +18,7 @@ setnames(ccfrdata, nms)
 recentdata <- subset(recentdata, select = c(date, p_cases_recent))
 
 plot(casedata$p_active_confirmed*100,type="l",lty=1,ylab="% active",
-     xlab="Days (cases considered active for 12 days)",main="Spain, Estimate of active COVID-19", ylim=c(0,2), xlim=c(1,210))
+     xlab="Days (cases considered active for 12 days)",main="Spain, Estimate of active COVID-19", ylim=c(0,1.5), xlim=c(114,200))
 
 lines(ccfrdata$p_active_ccfr*100,lty=1,col="red")
 
@@ -25,7 +26,7 @@ recentdata <- merge(casedata, recentdata, by = "date")
 
 recentdata <- subset(recentdata, select = c(X, p_cases_recent))
 
-lines(recentdata$p_cases_recent*100~recentdata$X,lty=1,col="blue")
+#lines(recentdata$p_cases_recent*100~recentdata$X,lty=1,col="blue")
 
 
 #facebook
@@ -47,7 +48,13 @@ coviddata <- fromJSON(response, flatten = TRUE) %>% data.frame()
 
 facebook <- c(rep(NA,114),coviddata$data.percent_cli)
 #facebook <- c(rep(NA,114),runMean(coviddata$data.percent_cli,5))
-lines(facebook*100,lty=2,col="green")
+lines(facebook*100,lty=1,col="green")
+
+facebook <- c(rep(NA,114),coviddata$data.percent_cli_unw)
+lines(facebook*100,lty=1,col="orange")
+
+facebook <- c(rep(NA,114),runmean(coviddata$data.percent_cli_unw,7))
+lines(facebook*100,lty=1,col="blue")
 
 # adding url
 path <- "https://covidmap.umd.edu/api/resources?indicator=covid&type=smoothed&country=Spain&daterange=20200423-20200723"
